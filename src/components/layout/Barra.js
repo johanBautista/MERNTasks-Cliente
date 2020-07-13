d7066405d5fe39e001585180b33866f765ff4996
johan Bautista <jsbparra@gmail.com>
2020-07-13 22:12:52 +0200

    form nuevoProyecto

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Barra = () => {
   return (
-    <div>
-      <h1>desde barra</h1>
-    </div>
+    <header className="app-header">
+      <p className="nombre-usuario">
+        Hola <span>Fernando </span>
+      </p>
+      <nav className="nav-principal">
+        <button className="btn btn-blank cerrar-sesion">Cerrar Sesión</button>
+      </nav>{' '}
+    </header>
   );
 };
 
--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useState } from 'react';
 
 const NuevoProyecto = () => {
+  //
+  const [proyecto, guardarProyecto] = useState({
+    nombre: '',
+  });
+
+  //
+  const { nombre } = proyecto;
+
+  //
+  const onChangeProyecto = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //
+  const onSubmitProyecto = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>desde nuevo proyecto</h1>
-    </div>
+    <Fragment>
+      <button type="button" className="btn btn-block btn-primario">
+        Nuevo Proyecto
+      </button>
+
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Nombre Proyecto"
+          name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
+        />
+
+        <input
+          type="submit"
+          className="btn btn-primario btn-block"
+          value="Agregar Proyecto"
+        />
+      </form>
+    </Fragment>
   );
 };
 
