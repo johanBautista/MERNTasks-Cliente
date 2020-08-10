26ba6488c7f05f594b62e2551c9cb69bbcdc8932
johan Bautista <jsbparra@gmail.com>
2020-08-10 09:08:30 +0200

    autenticacion initialstate

--- src/context/autenticacion/authReducer.js
+++ src/context/autenticacion/authReducer.js
@@ -1,4 +1,11 @@
-import {} from '../../types';
+import {
+  REGISTRO_EXITOSO,
+  REGISTRO_ERROR,
+  OBTENER_USUARIO,
+  LOGIN_EXITOSO,
+  LOGIN_ERROR,
+  CERRAR_SESION,
+} from '../../types';
 
 export default (state, action) => {
   switch (action.type) {
